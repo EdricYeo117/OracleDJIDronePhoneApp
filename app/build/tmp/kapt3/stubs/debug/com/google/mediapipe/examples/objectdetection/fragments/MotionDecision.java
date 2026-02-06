5bb501721493ef78b20bd397282405a2331a2fdc
YJR117 <133361144+EdricYeo117@users.noreply.github.com>
2026-02-06 16:46:29 +0800

    Updated detection gate

---
+++ app/build/tmp/kapt3/stubs/debug/com/google/mediapipe/examples/objectdetection/fragments/MotionDecision.java
@@ -0,0 +1,99 @@
+package com.google.mediapipe.examples.objectdetection.fragments;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\u0007\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u001a\n\u0002\u0010\u000e\n\u0000\b\u0086\b\u0018\u00002\u00020\u0001B?\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0006\u0012\u0006\u0010\u0007\u001a\u00020\b\u0012\b\u0010\t\u001a\u0004\u0018\u00010\n\u0012\u0006\u0010\u000b\u001a\u00020\b\u0012\u0006\u0010\f\u001a\u00020\b\u00a2\u0006\u0002\u0010\rJ\t\u0010\u0019\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001a\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001b\u001a\u00020\u0006H\u00c6\u0003J\t\u0010\u001c\u001a\u00020\bH\u00c6\u0003J\u000b\u0010\u001d\u001a\u0004\u0018\u00010\nH\u00c6\u0003J\t\u0010\u001e\u001a\u00020\bH\u00c6\u0003J\t\u0010\u001f\u001a\u00020\bH\u00c6\u0003JQ\u0010 \u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00062\b\b\u0002\u0010\u0007\u001a\u00020\b2\n\b\u0002\u0010\t\u001a\u0004\u0018\u00010\n2\b\b\u0002\u0010\u000b\u001a\u00020\b2\b\b\u0002\u0010\f\u001a\u00020\bH\u00c6\u0001J\u0013\u0010!\u001a\u00020\u00032\b\u0010\"\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010#\u001a\u00020\bH\u00d6\u0001J\t\u0010$\u001a\u00020%H\u00d6\u0001R\u0011\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\u000fR\u0011\u0010\u0007\u001a\u00020\b\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\u0011R\u0013\u0010\t\u001a\u0004\u0018\u00010\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u0013R\u0011\u0010\f\u001a\u00020\b\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0011R\u0011\u0010\u000b\u001a\u00020\b\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0011R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0016\u0010\u0017R\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0018\u0010\u0017\u00a8\u0006&"}, d2 = {"Lcom/google/mediapipe/examples/objectdetection/fragments/MotionDecision;", "", "motionFrame", "", "triggered", "changedPixelRatio", "", "consecutiveMotionFrames", "", "maskBitmap", "Landroid/graphics/Bitmap;", "maskWidth", "maskHeight", "(ZZFILandroid/graphics/Bitmap;II)V", "getChangedPixelRatio", "()F", "getConsecutiveMotionFrames", "()I", "getMaskBitmap", "()Landroid/graphics/Bitmap;", "getMaskHeight", "getMaskWidth", "getMotionFrame", "()Z", "getTriggered", "component1", "component2", "component3", "component4", "component5", "component6", "component7", "copy", "equals", "other", "hashCode", "toString", "", "app_debug"})
+public final class MotionDecision {
+    private final boolean motionFrame = false;
+    private final boolean triggered = false;
+    private final float changedPixelRatio = 0.0F;
+    private final int consecutiveMotionFrames = 0;
+    @org.jetbrains.annotations.Nullable()
+    private final android.graphics.Bitmap maskBitmap = null;
+    private final int maskWidth = 0;
+    private final int maskHeight = 0;
+    
+    public final boolean component1() {
+        return false;
+    }
+    
+    public final boolean component2() {
+        return false;
+    }
+    
+    public final float component3() {
+        return 0.0F;
+    }
+    
+    public final int component4() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final android.graphics.Bitmap component5() {
+        return null;
+    }
+    
+    public final int component6() {
+        return 0;
+    }
+    
+    public final int component7() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.google.mediapipe.examples.objectdetection.fragments.MotionDecision copy(boolean motionFrame, boolean triggered, float changedPixelRatio, int consecutiveMotionFrames, @org.jetbrains.annotations.Nullable()
+    android.graphics.Bitmap maskBitmap, int maskWidth, int maskHeight) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public MotionDecision(boolean motionFrame, boolean triggered, float changedPixelRatio, int consecutiveMotionFrames, @org.jetbrains.annotations.Nullable()
+    android.graphics.Bitmap maskBitmap, int maskWidth, int maskHeight) {
+        super();
+    }
+    
+    public final boolean getMotionFrame() {
+        return false;
+    }
+    
+    public final boolean getTriggered() {
+        return false;
+    }
+    
+    public final float getChangedPixelRatio() {
+        return 0.0F;
+    }
+    
+    public final int getConsecutiveMotionFrames() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final android.graphics.Bitmap getMaskBitmap() {
+        return null;
+    }
+    
+    public final int getMaskWidth() {
+        return 0;
+    }
+    
+    public final int getMaskHeight() {
+        return 0;
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/google/mediapipe/examples/objectdetection/fragments/MotionGate.java
@@ -0,0 +1,34 @@
+package com.google.mediapipe.examples.objectdetection.fragments;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00006\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u0007\n\u0002\b\u0004\n\u0002\u0010\t\n\u0002\b\u0004\n\u0002\u0010\u0014\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001BU\u0012\b\b\u0002\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0005\u0012\b\b\u0002\u0010\u0006\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0007\u001a\u00020\u0005\u0012\b\b\u0002\u0010\b\u001a\u00020\u0003\u0012\b\b\u0002\u0010\t\u001a\u00020\n\u0012\b\b\u0002\u0010\u000b\u001a\u00020\u0005\u0012\b\b\u0002\u0010\f\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\rJ\u0018\u0010\u0015\u001a\u00020\u00162\u0006\u0010\u0017\u001a\u00020\u00182\b\b\u0002\u0010\u0019\u001a\u00020\nR\u000e\u0010\u0004\u001a\u00020\u0005X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u000e\u001a\u0004\u0018\u00010\u000fX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0010\u001a\u00020\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0011\u001a\u00020\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0012\u001a\u00020\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\t\u001a\u00020\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0013\u001a\u00020\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000b\u001a\u00020\u0005X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0014\u001a\u00020\nX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\b\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\u0005X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\f\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001a"}, d2 = {"Lcom/google/mediapipe/examples/objectdetection/fragments/MotionGate;", "", "downsampleStep", "", "alpha", "", "diffThreshold", "ratioThreshold", "minConsecutive", "cooldownMs", "", "globalChangeIgnoreRatio", "warmupFrames", "(IFIFIJFI)V", "bg", "", "bgH", "bgW", "consecutive", "frameCount", "lastTriggerMs", "update", "Lcom/google/mediapipe/examples/objectdetection/fragments/MotionDecision;", "image", "Landroidx/camera/core/ImageProxy;", "nowMs", "app_debug"})
+public final class MotionGate {
+    private final int downsampleStep = 0;
+    private final float alpha = 0.0F;
+    private final int diffThreshold = 0;
+    private final float ratioThreshold = 0.0F;
+    private final int minConsecutive = 0;
+    private final long cooldownMs = 0L;
+    private final float globalChangeIgnoreRatio = 0.0F;
+    private final int warmupFrames = 0;
+    @org.jetbrains.annotations.Nullable()
+    private float[] bg;
+    private int bgW = 0;
+    private int bgH = 0;
+    private int consecutive = 0;
+    private long lastTriggerMs = 0L;
+    private int frameCount = 0;
+    
+    public MotionGate(int downsampleStep, float alpha, int diffThreshold, float ratioThreshold, int minConsecutive, long cooldownMs, float globalChangeIgnoreRatio, int warmupFrames) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.google.mediapipe.examples.objectdetection.fragments.MotionDecision update(@org.jetbrains.annotations.NotNull()
+    androidx.camera.core.ImageProxy image, long nowMs) {
+        return null;
+    }
+    
+    public MotionGate() {
+        super();
+    }
+}
